5041325ebcc7d64a0cc84129f633185eda890133
Dinusha Nivanthaka Amerasinghe <dinusha@dinusha-HP-Pro-3130-Microtower-PC>
2016-05-31 17:12:37 +0530

    Updated the sources

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <div id="postContainer">
         <div id="posting">
         <form>
-        <input type="text" size="50" name="tweet" value="" />
+        <textarea cols="60" rows="2"></textarea>
         <input type="submit" name="post" value="Post" />
         </form>
         </div>
@@ -29,7 +29,7 @@
             <div class="avatar"></div>
             <div class="postContent">
                 <h3>Handle</h3>
-                <p class="message">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fringilla finibus justo, et varius purus tincidunt sed. Vestibulum sed condimentum mauris.</p>
+                <p class="message">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fringilla finibus justo, et varius purus tincidunt sed. Vestibulum sed condimentum mauris.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fringilla finibus justo, et varius purus tincidunt sed. Vestibulum sed condimentum mauris.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fringilla finibus justo, et varius purus tincidunt sed. Vestibulum sed condimentum mauris.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fringilla finibus justo, et varius purus tincidunt sed. Vestibulum sed condimentum mauris.</p>
             </div>
             <div class="clear"></div>
         </div>
